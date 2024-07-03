4e20a70dc5629bc2581ca1a70f38d7c63b56a808
Mr.Robot <elisiomassaqui8@gmail.com>
2024-07-03 19:28:12 +0100

    hi

---
+++ server.js
@@ -0,0 +1,45 @@
+const { exec, execSync } = require('child_process');
+
+//Relevantes
+//Habilitar a saída verbose produzirá informações mais detalhadas no console .
+const verbose = '-v';
+//Armazenar porta detectada, e selecionar e conectar a primeira porta detectada.
+var porta = 'COM19';
+//Familia arduino, aqui tá tudo bem vou usar arduino uno apenas.
+var uno = 'arduino:avr:uno';
+
+// Defina o caminho do sketch aqui
+var sketchPath = 'C:\\Users\\Administrador\\Documents\\Causa-Efeito, SINER\\Wandi Robot\\mundo';
+
+// Comando para o arduino-cli
+
+//Checar versão
+const arduinoVersion = 'arduino-cli version';
+//Atualizar.
+const arduinoAtualizar = 'arduino-cli core update-index';
+//Instalar familia avr
+const arduinoAVR = 'arduino-cli core install arduino:avr';
+//plcas de dispositivos conectados ao PC.
+const placasDisponiveis = 'arduino-cli board list'
+//Todas placas.
+const todasplacas = 'arduino-cli board listall'
+//Olhar todos tipos de placa suportada.
+const arduinoCore = 'arduino-cli core search';
+//Compilar codigos.
+const compilarCode = `arduino-cli compile --fqbn "${uno}" -p "${porta}" "${sketchPath}" ${verbose}`;
+//Enviar codigos pra placa.
+const enviarCode = `arduino-cli upload --fqbn "${uno}" -p "${porta}" "${sketchPath}" ${verbose}`;
+
+var executeComando = arduinoAtualizar;
+
+// Executa o comando
+exec(executeComando, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Erro:\n ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`stderr info:\n ${stderr}`);
+  }
+  console.log(`Saída:\n  ${stdout}`);
+});

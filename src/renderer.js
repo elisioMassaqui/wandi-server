f2f71b6a541f9b80a95d0036a5acce8c6dd98652
Mr.Robot <elisiomassaqui8@gmail.com>
2024-07-16 08:17:26 +0100

    ajuste do console e os comandos arduino-cli funcionam agora porque mudamos de usuario do pc, tinha muito conflito no anterior e tamém baixamos o instalador msi que já coloca o arduino-cli nas variaveis de ambiente

--- src/renderer.js
+++ src/renderer.js
@@ -2,17 +2,17 @@
 const { exec } = require('child_process');
 
 document.getElementById('execute-btn').addEventListener('click', () => {
-    exec('arduino-cli board', (error, stdout, stderr) => {
+    exec('arduino-cli', (error, stdout, stderr) => {
         if (error) {
             console.error(`Erro ao listar diretório: ${error}`);
-            document.querySelector('.flexbox-item.item-1').innerHTML = `<pre>${error}</pre>`;
+            document.querySelector('.console').innerHTML = `<pre>${error}</pre>`;
             return;
         }
         if (stderr) {
             console.error(`stderr: ${stderr}`);
-            document.querySelector('.flexbox-item.item-1').innerHTML = `<pre>${stderr}</pre>`;
+            document.querySelector('console').innerHTML = `<pre>${stderr}</pre>`;
             return;
         }
-        document.querySelector('.flexbox-item.item-1').innerHTML = `<pre>${stdout}</pre>`;
+        document.querySelector('.console').innerHTML = `<pre>${stdout}</pre>`;
     });
 });
